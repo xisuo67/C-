a080b2a49bbd215b4924515302f3a313ad9cfa67
xisuo67 <392706283@qq.com>
2018-03-16 17:27:39 +0800

    日常

--- 批量导入/ImportDemo/ImportDemo/Controllers/ImportController.cs
+++ 批量导入/ImportDemo/ImportDemo/Controllers/ImportController.cs
@@ -14,30 +14,32 @@ namespace ImportDemo.Controllers
         // GET: Import
         public ActionResult Index()
         {
-            var a = Query();
             return View();
         }
         /// <summary>
         /// 查询学生信息
         /// </summary>
         /// <returns></returns>
-        public ApiResult<List<Students>> Query()
+        public JsonResult Query()
         {
+            JsonResult result = new JsonResult();
             ApiResult<List<Students>> apiResult = new ApiResult<List<Students>>();
             string sql = "select * from students";
             try
             {
+
                 var students = DBHelper.GetDataTable(sql).ToList<Students>();
-                apiResult.ErrorCode = 0;
-                apiResult.Result = students;
-                apiResult.TotalCount = students.Count();
+                return Json(students);
+                //apiResult.ErrorCode = 0;
+                //apiResult.Result = students;
+                //apiResult.TotalCount = students.Count();
             }
             catch (Exception ex)
             {
                 apiResult.ErrorCode = 1001;
                 apiResult.Message = ex.ToString();
             }
-            return apiResult;
+            return result;
         }
 
     }
--- 批量导入/ImportDemo/ImportDemo/Script/import.js
+++ 批量导入/ImportDemo/ImportDemo/Script/import.js
@@ -1,6 +1,17 @@
 ﻿ImportFunction = (function () {
     initGrid = function () {
-        //alert("1");
+        GridList = $("#ListTable").jqGrid({
+            url: "../Import/Query",
+            datatype: "json",
+            colNames: ['学生编号', '学生姓名', '年龄', '性别'],
+            colModel: [
+                { name: 'SNO', index: 'SNO', width: 55 ,align: 'center'},
+                { name: 'SNAME', index: 'SNAME', width: 90 ,align: 'center'},
+                { name: 'AGE', index: 'AGE', width: 100, align: 'center'},
+                { name: 'SEX', index: 'SEX', width: 80, align: "center" }
+            ],
+            sortorder: "desc",
+        });
     };
     return {
         init: function () {

d803400d74245741c6853c08feb571a13209fa66
xisuo67 <392706283@qq.com>
2018-03-18 16:41:11 +0800

    导入方法实现

---
+++ 批量导入/ImportDemo/ImportDemo/Util/StudentExcelImport.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Data;
+using System.Linq;
+using Utility;
+
+namespace ImportDemo.Util
+{
+    /// <summary>
+    /// 学生信息批量导入
+    /// </summary>
+    [Export(typeof(ExcelImport))]
+    public class StudentExcelImport : ExcelImport
+    {
+        private static Dictionary<string, ImportVerify> dictFields = new List<ImportVerify>
+        {
+
+        }.ToDictionary(e => e.ColumnName, e => e);
+        #region "override方法"
+        /// <summary>
+        /// 业务类型
+        /// </summary>
+        public override ExcelImportType Type => ExcelImportType.StudentsInfo;
+        /// <summary>
+        /// Excel字段映射及校验缓存
+        /// </summary>
+        public override Dictionary<string, ImportVerify> DictFields => dictFields;
+        /// <summary>
+        /// 保存导入信息
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <param name="extraInfo"></param>
+        /// <returns></returns>
+        public override object SaveImportData(DataTable dt, Dictionary<string, object> extraInfo)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    #endregion
+}
